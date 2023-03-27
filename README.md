53b65946067dcf550e776f5315c247e9c9e3ddea
daniilshkliar <daniilshkliar@yandex.ru>
2023-03-27 21:46:18 +0300

    initial

---
+++ README.md
@@ -0,0 +1,18 @@
+Use "**npm install**" to install all required packages.
+
+Use "**npm run dev**" to start development server.
+
+Use "**npm run build**" to create a production build.
+
+--- with Redux ---
+
+npm i react-redux @reduxjs/toolkit
+
+--- with MobX ---
+
+npm i mobx mobx-react-lite mobx-state-tree
+
+--- with SCSS ---
+
+npm i -D sass sass-loader
+edit webpack.config.js > module.exports.rules
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/app/index.tsx"),
+  output: {
+    filename: "[name].[fullhash].js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: ["style-loader", "css-loader", "sass-loader"],
+      // },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
